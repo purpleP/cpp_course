f562b3c4255cd44fb6feeaab982ae3a7bbbe7708
Michael Doronin <warrior2031@mail.ru>
2019-12-02 07:46:08 +0300

    WIP

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,13 +1,45 @@
-#include <algorithm>
+#include <numeric>
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
 
+void const print_vector(vector<bool>& v) {
+    cerr << '[';
+    for (auto b : v) {
+        cerr << b << ", ";
+    }
+    cerr << ']';
+}
+
 int main() {
-    string s1, s2, s3;
-    cin >> s1 >> s2 >> s3;
-    cout << min({s1, s2, s3});
-    return 0;
+    vector<bool> queue;
+    unsigned int opsCount;
+    cin >> opsCount;
+    string operation;
+    int arg;
+    while (opsCount--) {
+        cin >> operation;
+        if (operation == "COME") {
+            cin >> arg;
+            if (arg >= 0) {
+                while (arg-- != 0) {
+                    queue.push_back(false);
+                }
+            }
+            else {
+                while (arg++ != 0) {
+                    queue.pop_back();
+                }
+            }
+        }
+        if (operation == "WORRY") {
+            cin >> arg;
+            queue[arg] = true;
+        }
+        if (operation == "WORRY_COUNT") {
+            unsigned int count = accumulate(begin(queue), end(queue), 0);
+            cout << count << endl;
+        }
+    }
 }
