c01bcc50e2fdcc030000ed805c2c4dbb5b01fc29
mikhail.doronin <mikhail.doronin@alibaba-inc.com>
2019-12-03 19:05:15 +0300

    WIP

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,12 +1,11 @@
-#include <iterator>
-#include <numeric>
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <set>
+#include <map>
 
 using namespace std;
 
-void const print_vector(vector<string>& v) {
+void print_vector(const vector<string>& v) {
     cout << '[';
     for (auto b : v) {
         cout << b << ", ";
@@ -14,59 +13,97 @@ void const print_vector(vector<string>& v) {
     cout << ']';
 }
 
-void const print_tasks(vector<string>& tasks) {
-    cout << tasks.size();
-    for (auto t : tasks) {
-        cout << ' ' << t;
+
+void dump(const map<string, string>& capitals) {
+    if (capitals.empty()) {
+        cout << "There are no countries in the world";
+    } else {
+        auto it = begin(capitals);
+        cout << it->first << '/' << it->second;
+        it++;
+        for (; it != end(capitals); it++) {
+            cout << ' ' << it->first << '/' << it->second;
+        }
     }
     cout << endl;
 }
 
-void update_state(
-    vector<vector<string>>& state,
-    unsigned short old_num_of_days,
-    unsigned short num_of_days
+void rename(
+    map<string, string>& capitals,
+    const string& old_name,
+    const string& new_name
 ) {
-    short diff = num_of_days - old_num_of_days;
-    if (diff == 0) { return; }
-    if (diff > 0) {
-        while (diff--) {
-            state.push_back(vector<string>());
-        }
-    } else {
-        vector<string>& temp = state[num_of_days - 1];
-        for (unsigned int i = num_of_days; i < state.size(); ++i) {
-            temp.insert(end(temp), begin(state[i]), end(state[i]));
-        }
-        while (diff++) {
-            state.pop_back();
-        }
-    }
+    string msg = "Incorrect rename, skip";
+    if (old_name == new_name) {cout << msg << endl; return;}
+    if (capitals.find(new_name)->first == new_name) {cout << msg << endl; return;}
+    auto it = capitals.find(old_name);
+    if (it == end(capitals)) { cout << msg << endl; return;}
+    string capital = it->second;
+    capitals.erase(it);
+    capitals[new_name] = capital;
+    cout
+        << "Country " << old_name
+        << " with capital " << it->second
+        << " has been renamed to " << new_name
+        << endl;
 }
 
+void insert_or_update(
+    map<string, string>& capitals,
+    const string& country,
+    const string& new_capital
+) {
+    auto [it, inserted] = capitals.insert({country, new_capital});
+    if (inserted) {
+        cout << "Introduce new country " << country << " with capital " << new_capital;
+    } else {
+        string old_capital = it->second;
+        cout << "Country " << country;
+        if (old_capital == new_capital) {
+            cout << " hasn't changed its capital";
+        } else {
+            cout
+                << " has changed its capital from " << old_capital
+                << " to " << new_capital;
+        }
+        it->second = new_capital;
+    }
+    cout << endl;
+}
+
+void about(const map<string, string>& capitals, const string& country) {
+    auto it = capitals.find(country);
+    cout << "Country " << country;
+    if (it == end(capitals)) {
+        cout << " doesn't exist";
+    } else {
+        cout << " has capital " << it->second;
+    }
+    cout << endl;
+}
+
+
 int main() {
-    unsigned short num_of_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    unsigned short index = 0;
-    unsigned int op_count;
-    vector<vector<string>> month_tasks(31);
-    month_tasks.assign(31, vector<string>());
-    cin >> op_count;
-    string operation;
-    unsigned int day_no;
-    while (op_count--) {
-        cin >> operation;
-        if (operation == "ADD") {
-            string task;
-            cin >> day_no >> task;
-            month_tasks[day_no - 1].push_back(task);
-        } else if (operation == "DUMP") {
-            cin >> day_no;
-            print_tasks(month_tasks[day_no - 1]);
-        } else if (operation == "NEXT") {
-            if (index == 11) { continue;}
-            unsigned short old_num_of_days = num_of_days[index];
-            update_state(month_tasks, old_num_of_days, num_of_days[++index]);
+    unsigned int n;
+    map<string, string> capitals;
+    cin >> n;
+    while (n--) {
+        string cmd;
+        cin >> cmd;
+        if (cmd == "CHANGE_CAPITAL") {
+            string country, capital;
+            cin >> country >> capital;
+            insert_or_update(capitals, country, capital);
+        } else if (cmd == "RENAME") {
+            string from, to;
+            cin >> from >> to;
+            rename(capitals, from, to);
+        } else if (cmd == "ABOUT") {
+            string country;
+            cin >> country;
+            about(capitals, country);
+        } else if (cmd == "DUMP") {
+            dump(capitals);
         }
     }
-    return 0;
 }
